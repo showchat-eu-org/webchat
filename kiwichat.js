de39827783ab98ca36da360f2faa9eb71732c1dd
KiwiChat <52504536+KiwiChat@users.noreply.github.com>
2024-08-31 14:45:01 +0200

    Create kiwichat.js

---
+++ kiwichat.js
@@ -0,0 +1,46 @@
+/* 
+ * KiwiChat https://showchat.eu.org by BaiatRau
+*/
+
+'use strict';
+
+function build_embed_src() {
+    // Variables
+    const base_url = 'https://kiwiirc.com/nextclient/';
+	const plugins = 'conference';
+	const theme = 'osprey';
+    const server_url = 'irc.radioclick.ro';
+	const server_port = '6667';
+	const chan = '#Romania,#RadioClick,#ShowChat';
+    const nick = 'KiwiChat??';
+	
+    // Get page URL information
+    let url = new URL(window.location);
+
+    // Construct the iframe src URL
+    let src = base_url;
+	if (plugins) {
+        src += '?plugins=' + plugins;
+    }
+	if (theme) {
+        src += '&theme=' + theme;
+    } 
+	if (server_url) {
+        src += '#irc://' + server_url;
+    }
+	if (server_port) {
+        src += ':' + server_port;
+    } 
+	 if (chan) {
+        src += '/' + chan;
+    }
+	if (nick) {
+        src += '?&nick=' + nick;
+    }
+    return src;
+}
+function change_embed_dimensions() {
+    let embed = document.getElementById('chat-iframe');
+    embed.setAttribute('height', window.innerHeight);
+    embed.setAttribute('width', window.innerWidth);
+}
